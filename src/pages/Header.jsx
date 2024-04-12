8330c8b6c81823ea01fd46ee3b5aa15299718217
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2024-04-11 23:42:26 -0300

    crição do início da página

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,29 +1,122 @@
 import { Link } from 'react-router-dom'
 import '@/assets/css/header.css'
-import {Card, CardContent, CardDescription, CardFooter, CardHeader,CardTitle,} from "@/components/ui/card"
-import { Button } from '../components/ui/button'
+
+import { Button } from "@/components/ui/button"
+import { CircleUser, Menu, Package2, Search } from "lucide-react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
 function Header() {
     return (
-        <>
-            <header>
-                <div className="header-content">
-                    <h4>Início</h4>
-                </div>
-                <CardContent></CardContent>
-                <div>
-
-                    <CardTitle>
-                    <p>Consultório Dra Carolina</p>
-                    </CardTitle>
-                </div>
-            </header>
-            <div className="navbar">
-                <Link to='/inicio' >Início</Link>
-                <Link to='pacientes/cadastro'>Novo Paciente</Link>
+        <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+            <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+                <Link
+                    href="#"
+                    className="flex items-center gap-2 text-lg font-semibold md:text-base"
+                >
+                    <Package2 className="h-6 w-6" />
+                    <span className="sr-only">Bem Estar Inc</span>
+                </Link>
+                <Link
+                    href="#"
+                    className="text-muted-foreground transition-colors hover:text-foreground"
+                >
+                    Início
+                </Link>
+                <Link
+                    href="#"
+                    className="text-muted-foreground transition-colors hover:text-foreground"
+                >
+                    Pacientes
+                </Link>
+                <Link
+                    href="#"
+                    className="text-muted-foreground transition-colors hover:text-foreground"
+                >
+                    Sobre
+                </Link>
+            </nav>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button
+                        variant="outline"
+                        size="icon"
+                        className="shrink-0 md:hidden"
+                    >
+                        <Menu className="h-5 w-5" />
+                        <span className="sr-only">Alternar o menu de navegação</span>
+                    </Button>
+                </SheetTrigger>
+                <SheetContent side="left">
+                    <nav className="grid gap-6 text-lg font-medium">
+                        <Link
+                            href="#"
+                            className="flex items-center gap-2 text-lg font-semibold"
+                        >
+                            <Package2 className="h-6 w-6" />
+                            <span className="sr-only">Bem Estar Inc</span>
+                        </Link>
+                        <Link
+                            href="#"
+                            className="text-muted-foreground hover:text-foreground"
+                        >
+                            Início
+                        </Link>
+                        <Link
+                            href="#"
+                            className="text-muted-foreground hover:text-foreground"
+                        >
+                            Pacientes
+                        </Link>
+                        <Link
+                            href="#"
+                            className="text-muted-foreground hover:text-foreground"
+                        >
+                            Sobre
+                        </Link>
+                        <Link href="#" className="hover:text-foreground">
+                            Configurações
+                        </Link>
+                    </nav>
+                </SheetContent>
+            </Sheet>
+            <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+                <form className="ml-auto flex-1 sm:flex-initial">
+                    <div className="relative">
+                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                        <Input
+                            type="search"
+                            placeholder="Procurar..."
+                            className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
+                        />
+                    </div>
+                </form>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="secondary" size="icon" className="rounded-full">
+                            <CircleUser className="h-5 w-5" />
+                            <span className="sr-only">Alternar o menu do usuário</span>
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Minha conta</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Configurações</DropdownMenuItem>
+                        <DropdownMenuItem>Suporte</DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <Link to="/"><DropdownMenuItem>Sair</DropdownMenuItem></Link>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
-
-        </>
+        </header>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,82 +1,78 @@
-import {Card, CardContent, CardDescription, CardFooter, CardHeader,CardTitle,} from "@/components/ui/card"
-import Header from "./Header"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { FaUser, FaCalendarAlt } from 'react-icons/fa';
+import Header from "@/pages/Header"
 
 export function Home() {
+
     return (
-    <body>
-        <main className="container mx-auto p-10">
+        <>
             <Header></Header>
-            <section>
-                <div className="grid grid-cols-3 gap-6">
-                    <div className="bg-slate-600">
+            <main className="container mx-auto p-10">
+                <section className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 mb-4">
+                    <div className="bg-white rounded-lg shadow-lg">
+                        <Card>
+
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-center text-lg font-semibold">Novos Pacientes</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex items-center justify-center py-6">
+                                <FaUser className="text-5xl text-blue-500 mr-4" />
+                                <p className="text-3xl font-semibold">6</p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                    <div className="bg-white rounded-lg shadow-lg">
                         <Card>
                             <CardHeader className="pb-2">
-                                <CardTitle className="text-center">Novos Pacientes</CardTitle>
+                                <CardTitle className="text-center text-lg font-semibold">Agendamentos Hoje</CardTitle>
                             </CardHeader>
-                            <CardContent>
-                                <div>
-                                    <img className=""></img>
-                                    <p>6</p>
-                                    </div>
+                            <CardContent className="flex items-center justify-center py-6">
+                                <FaCalendarAlt className="text-5xl text-green-500 mr-4" />
+                                <p className="text-3xl font-semibold">6</p>
                             </CardContent>
-                            </Card></div>
-                    <div className="bg-slate-600">
+                        </Card>
+                    </div>
+                    <div className="bg-white rounded-lg shadow-lg">
                         <Card>
-                            <CardHeader>
-                                <CardTitle className="text-center">Agendamentos Hoje</CardTitle>
-                                </CardHeader>
-                                <CardContent>
-                                    <div>
-                                        <img className=""></img>
-                                        <p>6</p>
-                                        </div>
-                                        </CardContent>
-                                        </Card></div>
-                                        <div className="bg-slate-600">
-                                            <Card>
-                                                <CardHeader>
-                                                    <CardTitle className="text-center">Agendamentos Hoje</CardTitle>
-                                                    </CardHeader>
-                                                    <CardContent>
-                                                        <div>
-                                                            <img className=""></img>
-                                                            <p>6</p>
-                                                            </div>
-                                                            </CardContent>
-                                                            </Card></div>
-                                                            </div>
-                                                            <div className="bg-slate-600">
-                                                                <Card>
-                                                                    <CardHeader>
-                                                                        <CardTitle className="text-center">Agendamentos Hoje</CardTitle>
-                                                                        </CardHeader>
-                                                                        <CardContent>
-                                                                            <div>
-                                                                                <img className=""></img>
-                                                                                <p>6</p>
-                                                                                </div>
-                                                                                </CardContent>
-                                                                                </Card></div>
-                                                                                <div className="bg-slate-600">
-                                                                                    <Card>
-                                                                                        <CardHeader>
-                                                                                            <CardTitle></CardTitle>
-                                                                                        </CardHeader>
-                                                                                        <CardContent>
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-center text-lg font-semibold">Agendamentos da Semana</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex items-center justify-center py-6">
+                                <FaCalendarAlt className="text-5xl text-green-500 mr-4" />
+                                <p className="text-3xl font-semibold">12</p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </section>
 
-                                                                                        </CardContent>
-                                                                                        </Card></div>
-                                                                                        <div className="bg-slate-600">
-                                                                                            <Card>
-                                                                                            <CardHeader>
-                                                                                                <CardTitle></CardTitle>
-                                                                                            </CardHeader>
-                                                                                            <CardContent>
+                <section className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-6 mb-4">
+                    <div className="bg-white rounded-lg shadow-lg">
+                        <Card>
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-center text-lg font-semibold">Novos Pacientes</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex items-center justify-center py-6">
+                                <FaUser className="text-5xl text-blue-500 mr-4" />
+                                <p className="text-3xl font-semibold">6</p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </section>
 
-                                                                                            </CardContent>
-                                                                                            </Card></div>
-                                                                                            </section>
-                                                                                            </main>
-                                                                                            </body>
+                <section className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-6 mb-4">
+                    <div className="bg-white rounded-lg shadow-lg">
+                        <Card>
+                            <CardHeader className="pb-2">
+                                <CardTitle className="text-center text-lg font-semibold">Novos Pacientes</CardTitle>
+                            </CardHeader>
+                            <CardContent className="flex items-center justify-center py-6">
+                                <FaUser className="text-5xl text-blue-500 mr-4" />
+                                <p className="text-3xl font-semibold">6</p>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
